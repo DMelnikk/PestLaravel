2dcef008e8a9786e95e881746dc1a73e8623a74d
DMelnikk <dmitry.melnik001@mail.ru>
2026-06-15 18:58:54 +0300

    Implemented test controller

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function show()
+    {
+        
+    }
+
+    public function update()
+    {
+        
+    }
+}
