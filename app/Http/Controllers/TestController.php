cb5ba16a438217bb0cdfdd6b1dad031fa57c30ff
DMelnikk <dmitry.melnik001@mail.ru>
2026-06-15 20:31:43 +0300

    dasds

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,18 +6,17 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function index()
-    {
-        
+    public function index() {
+
     }
 
     public function show()
     {
-        
+
     }
 
     public function update()
     {
-        
+
     }
 }
